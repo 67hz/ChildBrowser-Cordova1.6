a4b831d6820265257b48928fbaed16cd4835a8bb
Aaron Hinojosa <aaronjhinojosa@gmail.com>
2012-04-17 00:24:16 -0500

    1st commit

---
+++ ChildBrowserCommand.h
@@ -0,0 +1,30 @@
+//
+//  PhoneGap ! ChildBrowserCommand
+//
+//
+//  Created by Jesse MacFadyen on 10-05-29.
+//  Copyright 2010 Nitobi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#ifdef CORDOVA_FRAMEWORK
+	#import <Cordova/CDVPlugin.h>
+#else
+	#import "CDVPlugin.h"
+#endif
+#import "ChildBrowserViewController.h"
+
+
+
+@interface ChildBrowserCommand : CDVPlugin <ChildBrowserDelegate>  {
+
+	ChildBrowserViewController* childBrowser;
+}
+
+@property (nonatomic, retain) ChildBrowserViewController *childBrowser;
+
+
+- (void) showWebPage:(NSMutableArray*)arguments withDict:(NSMutableDictionary*)options;
+-(void) onChildLocationChange:(NSString*)newLoc;
+
+@end
